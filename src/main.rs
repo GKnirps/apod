c04670d385bfdb47d60cd329eca29bcd5c369bfb
Guido Knips <guido.knips@posteo.de>
2021-03-14 14:49:43 +0100

    Add handling of return type variations

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,23 @@ use std::fs::write;
 use std::path::PathBuf;
 
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Deserialize)]
+#[serde(tag = "media_type")]
+enum MediaType {
+    #[serde(rename = "image")]
+    Image { hdurl: String },
+    #[serde(rename = "video")]
+    Video {},
+}
+
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Deserialize)]
 struct ApodData {
-    copyright: String,
+    copyright: Option<String>,
     date: NaiveDate,
     explanation: String,
-    hdurl: String,
-    media_type: String,
     title: String,
     url: String,
+    #[serde(flatten)]
+    media: MediaType,
 }
 
 const USER_AGENT: &str = "I CAN HAZ STARS?";
@@ -32,13 +41,16 @@ fn fetch_current_data(client: &Client, api_key: &str) -> Result<ApodData, String
 }
 
 fn fetch_hd_image(client: &Client, image_data: &ApodData) -> Result<Bytes, String> {
-    client
-        .get(&image_data.hdurl)
-        .header(header::USER_AGENT, USER_AGENT)
-        .send()
-        .map_err(|e| format!("Error fetching image: {}", e))?
-        .bytes()
-        .map_err(|e| format!("Unable to read image: {}", e))
+    match &image_data.media {
+        MediaType::Image { hdurl: url } => client
+            .get(url)
+            .header(header::USER_AGENT, USER_AGENT)
+            .send()
+            .map_err(|e| format!("Error fetching image: {}", e))?
+            .bytes()
+            .map_err(|e| format!("Unable to read image: {}", e)),
+        MediaType::Video {} => Err("Unable to fetch image, media type is video".to_owned()),
+    }
 }
 
 fn write_image(mut dir: PathBuf, apod_data: &ApodData, image: &[u8]) -> Result<(), String> {
@@ -60,4 +72,78 @@ fn main() -> Result<(), String> {
 
     // TODO: make directory configurable
     write_image(PathBuf::from("."), &apod_data, &hd_image)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json::{from_str, json};
+
+    #[test]
+    fn test_media_type_image_deserialization() {
+        // given
+        let json = json! ({
+            "copyright": "Nicolas Lefaudeux",
+            "date": "2021-03-08",
+            "explanation": "What created the unusual red tail[…]",
+            "hdurl": "https://apod.nasa.gov/apod/image/2103/Neowise3Tails_Lefaudeux_1088.jpg",
+            "media_type": "image",
+            "service_version": "v1",
+            "title": "Three Tails of Comet NEOWISE",
+            "url": "https://apod.nasa.gov/apod/image/2103/Neowise3Tails_Lefaudeux_960.jpg"
+        })
+        .to_string();
+
+        // when
+        let parsed = from_str::<ApodData>(&json);
+
+        // then
+        let apod_data = parsed.expect("Expected successful parsing");
+        assert_eq!(
+            apod_data,
+            ApodData {
+                copyright: Some("Nicolas Lefaudeux".to_owned()),
+                date: NaiveDate::from_ymd(2021, 3, 8),
+                explanation: "What created the unusual red tail[…]".to_owned(),
+                title: "Three Tails of Comet NEOWISE".to_owned(),
+                url: "https://apod.nasa.gov/apod/image/2103/Neowise3Tails_Lefaudeux_960.jpg"
+                    .to_owned(),
+                media: MediaType::Image {
+                    hdurl: "https://apod.nasa.gov/apod/image/2103/Neowise3Tails_Lefaudeux_1088.jpg"
+                        .to_owned()
+                }
+            }
+        )
+    }
+
+    #[test]
+    fn test_media_type_video_deserialization() {
+        // given
+        let json = json! (  {
+            "date": "2021-03-09",
+            "explanation": "Is that a fossil?[…]",
+            "media_type": "video",
+            "service_version": "v1",
+            "title": "Perseverance 360: Unusual Rocks and the Search for Life on Mars",
+            "url": "https://mars.nasa.gov/layout/embed/image/mars-panorama/?id=25674"
+        })
+        .to_string();
+
+        // when
+        let parsed = from_str::<ApodData>(&json);
+
+        // then
+        let apod_data = parsed.expect("Expected successful parsing");
+        assert_eq!(
+            apod_data,
+            ApodData {
+                copyright: None,
+                date: NaiveDate::from_ymd(2021, 3, 9),
+                explanation: "Is that a fossil?[…]".to_owned(),
+                title: "Perseverance 360: Unusual Rocks and the Search for Life on Mars".to_owned(),
+                url: "https://mars.nasa.gov/layout/embed/image/mars-panorama/?id=25674".to_owned(),
+                media: MediaType::Video {},
+            }
+        )
+    }
 }
